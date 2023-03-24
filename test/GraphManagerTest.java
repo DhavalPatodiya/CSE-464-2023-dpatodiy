ce658fcabc88ddfefab53f9adfaea42792bbf9ce
DhavalPatodiya <patodiya25@gmail.com>
2023-03-23 23:42:36 -0700

    testing changes

--- test/GraphManagerTest.java
+++ test/GraphManagerTest.java
@@ -3,7 +3,6 @@ import org.junit.Before;
 import org.junit.Test;
 
 import javax.imageio.ImageIO;
-import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBuffer;
 import java.io.File;
@@ -138,7 +137,6 @@ public class GraphManagerTest {
         String expectedFile = "expected.png";
         String actualFile = "actual.png";
 
-        g.addNode("f");
         g.outputGraphics(actualFile, "png");
        // g.outputGraphics(expectedFile, "png");
 
@@ -166,42 +164,6 @@ public class GraphManagerTest {
         g.outputGraphics(actualFile, "jpg");
       //  g.outputGraphics(expectedFile, "jpg");
 
-
-        BufferedImage img1 = ImageIO.read(new File(expectedFile));
-        BufferedImage img2 = ImageIO.read(new File(actualFile));
-        int w1 = img1.getWidth();
-        int w2 = img2.getWidth();
-        int h1 = img1.getHeight();
-        int h2 = img2.getHeight();
-        if ((w1!=w2)||(h1!=h2)) {
-            Assert.assertTrue(w1==w2 && h1==h2);
-        } else {
-            long diff = 0;
-            for (int j = 0; j < h1; j++) {
-                for (int i = 0; i < w1; i++) {
-                    //Getting the RGB values of a pixel
-                    int pixel1 = img1.getRGB(i, j);
-                    Color color1 = new Color(pixel1, true);
-                    int r1 = color1.getRed();
-                    int g1 = color1.getGreen();
-                    int b1 = color1.getBlue();
-                    int pixel2 = img2.getRGB(i, j);
-                    Color color2 = new Color(pixel2, true);
-                    int r2 = color2.getRed();
-                    int g2 = color2.getGreen();
-                    int b2 = color2.getBlue();
-                    //sum of differences of RGB values of the two images
-                    long data = Math.abs(r1 - r2) + Math.abs(g1 - g2) + Math.abs(b1 - b2);
-                    diff = diff + data;
-                }
-            }
-            double avg = diff / (w1 * h1 * 3);
-            double percentage = (avg / 255) * 100;
-            Assert.assertTrue(percentage < 10);
-
-        }
-
-        /*
         BufferedImage actualImage = ImageIO.read(new File(actualFile));
         DataBuffer actualDataBuffer = actualImage.getData().getDataBuffer();
         int actualImageSize = actualDataBuffer.getSize();
@@ -216,8 +178,6 @@ public class GraphManagerTest {
                 Assert.assertEquals(expectedDataBuffer.getElem(i), actualDataBuffer.getElem(i));
             }
         }
-
-         */
     }
 
     @Test(expected= IOException.class)
